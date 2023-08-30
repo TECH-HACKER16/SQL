eda914e76189801a534048d3736bbefc84eeb516
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-08-30 12:51:41 +0530

    create simple database

---
+++ Simple Database.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE Market;
+
+USE Market;
+
+CREATE TABLE Vegetables
+(
+	Sno int primary KEY,
+    Item varchar(50),
+    Price smallint
+);
+
+INSERT INTO Vegetables 
+(Sno,Item,Price)
+
+VALUES
+	(1,"Tomatoes",40),
+	(2,"Brinjal",30),
+	(3,"Drumstick",20),
+	(4,"Carrot",50),
+	(5,"Potato",40);
+
+Select * from vegetables
