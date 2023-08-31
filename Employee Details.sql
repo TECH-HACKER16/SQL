f15d564accac5c59a3bb40a34a6515950458f9b7
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-08-31 22:45:47 +0530

    Logical and Special Operators

---
+++ Employee Details.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE IF NOT EXISTS Company;
+
+USE Company;
+-- drop table Employee_Details;
+CREATE TABLE Employee_Details
+(
+	Sno INT UNIQUE,
+	Employee_ID INT PRIMARY KEY,
+    Employee_Name VARCHAR(50),
+    Employee_Rating FLOAT,
+    Place_of_Living VARCHAR(50)
+    
+);
+
+INSERT INTO Employee_Details
+(Sno,Employee_ID,Employee_Name,Employee_Rating,Place_of_Living)
+
+VALUES
+	(1,1001,"George",7,"New York"),
+    (2,1002,"patrick",8,"Washington"),
+	(3,1003,"John",8,"New York"),
+    (4,1004,"David",7,"Queens"),
+    (5,1005,"Chadvick",9,"Paris");
+    
+SELECT * FROM Employee_Details WHERE Employee_Rating=8 AND Place_of_Living = "New York";
+
+-- REMOVE COMMENT TO RUN EACH LINE
+
+-- SELECT * FROM Employee_Details WHERE Employee_Rating=10 OR Place_of_Living = "New York";
+
+-- SELECT * FROM Employee_Details WHERE Place_of_Living NOT IN ("New York","Washington");
+
+-- SELECT * FROM Employee_Details WHERE Employee_Rating BETWEEN 6 AND 8;
+
+-- SELECT * FROM Employee_Details WHERE Employee_Rating=8 AND Place_of_Living = "New York";
+
+-- SELECT * FROM Employee_Details WHERE Place_of_Living IN ("Paris","New York");
+
+
+
+
+
+
