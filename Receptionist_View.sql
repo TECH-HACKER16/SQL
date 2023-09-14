5521572be38d19fc4c433f408a5aea8c0cb0e56c
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-09-14 11:27:40 +0530

    Creating virtual table using VIEW command

---
+++ Receptionist_View.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS Company;
+
+USE Company;
+
+CREATE TABLE Employee_details6
+(
+	Sno INT UNIQUE,
+	Employee_ID INT PRIMARY KEY,
+    Employee_Name VARCHAR(50),
+    Employee_Rating FLOAT,
+    Place_of_Living VARCHAR(50),
+    Employee_Salary FLOAT
+);
+
+INSERT INTO Employee_details6
+(Sno,Employee_ID,Employee_Name,Employee_Rating,Place_of_Living,Employee_Salary)
+
+VALUE
+	(1,1001,"George",7,"New York",40000),
+    (2,1002,"patrick",8,"Washington",50000),
+	(3,1003,"John",8,"New York",30000),
+    (4,1004,"David",7,"Queens",35000),
+    (5,1005,"Chadvick",9,"Paris",55000);
+
+CREATE VIEW Receptionist_View AS 
+SELECT Employee_ID,Employee_Name FROM Employee_details6;
+
+SELECT * FROM Receptionist_View;
+
+
+
